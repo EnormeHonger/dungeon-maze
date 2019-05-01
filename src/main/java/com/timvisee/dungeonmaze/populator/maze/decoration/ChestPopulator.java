bb4e6a8ececdb4a5fb793863e9d7d2c56d3553bd
EnormeHonger <fabianaukema@gmail.com>
2019-05-01 22:18:24 +0000

    Update ChestPopulator.java

--- src/main/java/com/timvisee/dungeonmaze/populator/maze/decoration/ChestPopulator.java
+++ src/main/java/com/timvisee/dungeonmaze/populator/maze/decoration/ChestPopulator.java
@@ -19,178 +19,129 @@ import com.timvisee.dungeonmaze.util.ChestUtils;
 
 public class ChestPopulator extends MazeRoomBlockPopulator {
 
-    /** General populator constants. */
+	/** General populator constants. */
 	private static final int LAYER_MIN = 1;
 	private static final int LAYER_MAX = 7;
 	private static final float ROOM_CHANCE = .03f;
 
-    // TODO: Implement this!
+	// TODO: Implement this!
 	public static final double CHANCE_CHEST_ADDITION_EACH_LEVEL = -0.333; // to 1
-
 	@Override
 	public void populateRoom(MazeRoomBlockPopulatorArgs args) {
-        final Chunk chunk = args.getSourceChunk();
-        final Random rand = args.getRandom();
-        final int x = args.getRoomChunkX();
-        final int z = args.getRoomChunkZ();
-        final int chestX = x + rand.nextInt(6) + 1;
-        final int chestY = args.getFloorY() + 1;
-        final int chestZ = z + rand.nextInt(6) + 1;
-	// magic number 75 because this is the highest y where dungeon can generate
-	final int chestLevel = 75 - chestY;
-	// calculate extra loot
-	int extraLoot = 0;
-	for (int i = 0; i < chestLevel; i = i + 7)
+		final Chunk chunk = args.getSourceChunk();
+		final Random rand = args.getRandom();
+		final int x = args.getRoomChunkX();
+		final int z = args.getRoomChunkZ();
+		final int chestX = x + rand.nextInt(6) + 1;
+		final int chestY = args.getFloorY() + 1;
+		final int chestZ = z + rand.nextInt(6) + 1;
+		// magic number 75 because this is the highest y where dungeon can generate
+		final int chestLevel = 75 - chestY;
+		// calculate extra loot
+		int extraLoot = 0;
+		for (int i = 0; i < chestLevel; i = i + 7)
 		extraLoot++;
-	
-	
 
-        if(!(chunk.getBlock(chestX, chestY - 1, chestZ).getType() == Material.AIR)) {
-            Block chestBlock = chunk.getBlock(chestX, chestY, chestZ);
-            if(chestBlock.getType() == Material.AIR) {
+		if (! (chunk.getBlock(chestX, chestY - 1, chestZ).getType() == Material.AIR)) {
+			Block chestBlock = chunk.getBlock(chestX, chestY, chestZ);
+			if (chestBlock.getType() == Material.AIR) {
 
-                // Generate new inventory contents
-                List<ItemStack> contents = generateChestContents(rand);
-                chestBlock.setType(Material.CHEST);
+				// Generate new inventory contents
+				List < ItemStack > contents = generateChestContents(rand, extraLoot);
+				chestBlock.setType(Material.CHEST);
 
-                // Call the chest generation event
-                GenerationChestEvent event = new GenerationChestEvent(chestBlock, rand, contents, MazeStructureType.UNSTRUCTURE);
-                Bukkit.getServer().getPluginManager().callEvent(event);
+				// Call the chest generation event
+				GenerationChestEvent event = new GenerationChestEvent(chestBlock, rand, contents, MazeStructureType.UNSTRUCTURE);
+				Bukkit.getServer().getPluginManager().callEvent(event);
 
-                // Do the event
-                if(!event.isCancelled()) {
-                    // Make sure the chest is still there, a developer could change the chest through the event!
-                    if(event.getBlock().getType() != Material.CHEST)
-                        return;
+				// Do the event
+				if (!event.isCancelled()) {
+					// Make sure the chest is still there, a developer could change the chest through the event!
+					if (event.getBlock().getType() != Material.CHEST) return;
 
-                    // Add the contents to the chest
-                    ChestUtils.addItemsToChest(event.getBlock(), event.getContents(), !event.getAddContentsInOrder(), rand);
-                } else {
-                    // The event is cancelled
-                    // Put the chest back to it's original state (air)
-                    chestBlock.setType(Material.AIR);
-                }
+					// Add the contents to the chest
+					ChestUtils.addItemsToChest(event.getBlock(), event.getContents(), !event.getAddContentsInOrder(), rand);
+				} else {
+					// The event is cancelled
+					// Put the chest back to it's original state (air)
+					chestBlock.setType(Material.AIR);
+				}
 
-            } else if (chestBlock.getType() == Material.CHEST) {
-                // The follow is for rare case when the chest is generate before the plugin does the event
-                Chest chest = (Chest) chestBlock.getState();
-                if (chest.getInventory() != null) {
-                    // Generate new inventory contents
-                    List<ItemStack> contents = generateChestContents(rand);
+			} else if (chestBlock.getType() == Material.CHEST) {
+				// The follow is for rare case when the chest is generate before the plugin does the event
+				Chest chest = (Chest) chestBlock.getState();
+				if (chest.getInventory() != null) {
+					// Generate new inventory contents
+					List < ItemStack > contents = generateChestContents(rand);
 
-                    // Call the chest generation event
-                    GenerationChestEvent event = new GenerationChestEvent(chestBlock, rand, contents, MazeStructureType.UNSTRUCTURE);
-                    Bukkit.getServer().getPluginManager().callEvent(event);
+					// Call the chest generation event
+					GenerationChestEvent event = new GenerationChestEvent(chestBlock, rand, contents, MazeStructureType.UNSTRUCTURE);
+					Bukkit.getServer().getPluginManager().callEvent(event);
 
-                    // Do the event
-                    if(!event.isCancelled()) {
-                        // Make sure the chest is still there, a developer could change the chest through the event!
-                        if(event.getBlock().getType() != Material.CHEST)
-                            return;
+					// Do the event
+					if (!event.isCancelled()) {
+						// Make sure the chest is still there, a developer could change the chest through the event!
+						if (event.getBlock().getType() != Material.CHEST) return;
 
-                        // Add the contents to the chest
-                        ChestUtils.addItemsToChest(event.getBlock(), event.getContents(), !event.getAddContentsInOrder(), rand);
-                    }
-                }
-            }
+						// Add the contents to the chest
+						ChestUtils.addItemsToChest(event.getBlock(), event.getContents(), !event.getAddContentsInOrder(), rand);
+					}
+				}
+			}
 		}
 	}
 
-	private List<ItemStack> generateChestContents(Random random) {
+	private List < ItemStack > generateChestContents(Random random, int extraLoot) {
 		// TODO: Use class for this, to also add feature to re loot chests
-        // Create a list to put the item stacks in
-		List<ItemStack> items = new ArrayList<>();
+		// Create a list to put the item stacks in
+		List < ItemStack > items = new ArrayList < >();
 
-        // Add items to the stack
-		if(random.nextInt(100) < 80)
-			items.add(new ItemStack(Material.TORCH, 4, (short) 0));
-		if(random.nextInt(100) < 40)
-			items.add(new ItemStack(Material.TORCH, 8, (short) 0));
-		if(random.nextInt(100) < 20)
-			items.add(new ItemStack(Material.TORCH, 12, (short) 0));
-		if(random.nextInt(100) < 40)
-			items.add(new ItemStack(Material.APPLE, 1, (short) 0));
-		if(random.nextInt(100) < 10)
-			items.add(new ItemStack(Material.ARROW, 16, (short) 0));
-		if(random.nextInt(100) < 5)
-			items.add(new ItemStack(Material.ARROW, 24, (short) 0));
-		if(random.nextInt(100) < 20)
-			items.add(new ItemStack(Material.DIAMOND, 1, (short) 0));
-		if(random.nextInt(100) < 50)
-			items.add(new ItemStack(Material.IRON_INGOT, 1, (short) 0));
-		if(random.nextInt(100) < 60)
-			items.add(new ItemStack(Material.GOLD_INGOT, 1, (short) 0));
-		if(random.nextInt(100) < 10)
-			items.add(new ItemStack(Material.IRON_SWORD, 1, (short) 0));
-		if(random.nextInt(100) < 40)
-			items.add(new ItemStack(Material.WOOD_SWORD, 1, (short) 0));
-		if(random.nextInt(100) < 20)
-			items.add(new ItemStack(Material.STONE_SWORD, 1, (short) 0));
-		if(random.nextInt(100) < 80)
-			items.add(new ItemStack(Material.WHEAT, 1, (short) 0));
-		if(random.nextInt(100) < 10)
-			items.add(new ItemStack(Material.WHEAT, 2, (short) 0));
-		if(random.nextInt(100) < 5)
-			items.add(new ItemStack(Material.WHEAT, 3, (short) 0));
-		if(random.nextInt(100) < 20)
-			items.add(new ItemStack(Material.BREAD, 1, (short) 0));
-		if(random.nextInt(100) < 20)
-			items.add(new ItemStack(Material.LEATHER_HELMET, 1, (short) 0));
-		if(random.nextInt(100) < 20)
-			items.add(new ItemStack(Material.LEATHER_CHESTPLATE, 1, (short) 0));
-		if(random.nextInt(100) < 20)
-			items.add(new ItemStack(Material.LEATHER_LEGGINGS, 1, (short) 0));
-		if(random.nextInt(100) < 20)
-			items.add(new ItemStack(Material.LEATHER_BOOTS, 1, (short) 0));
-		if(random.nextInt(100) < 40)
-			items.add(new ItemStack(Material.CHAINMAIL_HELMET, 1, (short) 0));
-		if(random.nextInt(100) < 40)
-			items.add(new ItemStack(Material.CHAINMAIL_CHESTPLATE, 1, (short) 0));
-		if(random.nextInt(100) < 40)
-			items.add(new ItemStack(Material.CHAINMAIL_LEGGINGS, 1, (short) 0));
-		if(random.nextInt(100) < 40)
-			items.add(new ItemStack(Material.CHAINMAIL_BOOTS, 1, (short) 0));
-		if(random.nextInt(100) < 10)
-			items.add(new ItemStack(Material.IRON_HELMET, 1, (short) 0));
-		if(random.nextInt(100) < 10)
-			items.add(new ItemStack(Material.IRON_CHESTPLATE, 1, (short) 0));
-		if(random.nextInt(100) < 10)
-			items.add(new ItemStack(Material.IRON_LEGGINGS, 1, (short) 0));
-		if(random.nextInt(100) < 10)
-			items.add(new ItemStack(Material.IRON_BOOTS, 1, (short) 0));
-		if(random.nextInt(100) < 30)
-			items.add(new ItemStack(Material.FLINT, 3, (short) 0));
-		if(random.nextInt(100) < 20)
-			items.add(new ItemStack(Material.FLINT, 5, (short) 0));
-		if(random.nextInt(100) < 10)
-			items.add(new ItemStack(Material.FLINT, 7, (short) 0));
-		if(random.nextInt(100) < 80)
-			items.add(new ItemStack(Material.PORK, 1, (short) 0));
-		if(random.nextInt(100) < 10)
-			items.add(new ItemStack(Material.GRILLED_PORK, 1, (short) 0));
-		if(random.nextInt(100) < 15)
-			items.add(new ItemStack(Material.REDSTONE, 5, (short) 0));
-		if(random.nextInt(100) < 10)
-			items.add(new ItemStack(Material.REDSTONE, 8, (short) 0));
-		if(random.nextInt(100) < 5)
-			items.add(new ItemStack(Material.REDSTONE, 13, (short) 0));
-		if(random.nextInt(100) < 3)
-			items.add(new ItemStack(Material.REDSTONE, 21, (short) 0));
-		if(random.nextInt(100) < 10)
-			items.add(new ItemStack(Material.COMPASS, 1, (short) 0));
-		if(random.nextInt(100) < 80)
-			items.add(new ItemStack(Material.RAW_FISH, 1, (short) 0));
-		if(random.nextInt(100) < 20)
-			items.add(new ItemStack(Material.COOKED_FISH, 1, (short) 0));
-		if(random.nextInt(100) < 20)
-			items.add(new ItemStack(Material.INK_SACK, 1, (short) 3));
-		if(random.nextInt(100) < 5)
-			items.add(new ItemStack(Material.CAKE, 1, (short) 0));
-		if(random.nextInt(100) < 80)
-			items.add(new ItemStack(Material.COOKIE, 3, (short) 0));
-		if(random.nextInt(100) < 20)
-			items.add(new ItemStack(Material.COOKIE, 5, (short) 0));
-		
+		// Add items to the stack
+		if (random.nextInt(100) < 80) items.add(new ItemStack(Material.TORCH, 4, (short) 0));
+		if (random.nextInt(100) < 40) items.add(new ItemStack(Material.TORCH, 8, (short) 0));
+		if (random.nextInt(100) < 20) items.add(new ItemStack(Material.TORCH, 12, (short) 0));
+		if (random.nextInt(100) < 40) items.add(new ItemStack(Material.APPLE, 1, (short) 0));
+		if (random.nextInt(100) < 10) items.add(new ItemStack(Material.ARROW, 16, (short) 0));
+		if (random.nextInt(100) < 5) items.add(new ItemStack(Material.ARROW, 24, (short) 0));
+		if (random.nextInt(100) < 20) items.add(new ItemStack(Material.DIAMOND, 1, (short) 0));
+		if (random.nextInt(100) < 50) items.add(new ItemStack(Material.IRON_INGOT, 1, (short) 0));
+		if (random.nextInt(100) < 60) items.add(new ItemStack(Material.GOLD_INGOT, 1, (short) 0));
+		if (random.nextInt(100) < 10) items.add(new ItemStack(Material.IRON_SWORD, 1, (short) 0));
+		if (random.nextInt(100) < 40) items.add(new ItemStack(Material.WOOD_SWORD, 1, (short) 0));
+		if (random.nextInt(100) < 20) items.add(new ItemStack(Material.STONE_SWORD, 1, (short) 0));
+		if (random.nextInt(100) < 80) items.add(new ItemStack(Material.WHEAT, 1, (short) 0));
+		if (random.nextInt(100) < 10) items.add(new ItemStack(Material.WHEAT, 2, (short) 0));
+		if (random.nextInt(100) < 5) items.add(new ItemStack(Material.WHEAT, 3, (short) 0));
+		if (random.nextInt(100) < 20) items.add(new ItemStack(Material.BREAD, 1, (short) 0));
+		if (random.nextInt(100) < 20) items.add(new ItemStack(Material.LEATHER_HELMET, 1, (short) 0));
+		if (random.nextInt(100) < 20) items.add(new ItemStack(Material.LEATHER_CHESTPLATE, 1, (short) 0));
+		if (random.nextInt(100) < 20) items.add(new ItemStack(Material.LEATHER_LEGGINGS, 1, (short) 0));
+		if (random.nextInt(100) < 20) items.add(new ItemStack(Material.LEATHER_BOOTS, 1, (short) 0));
+		if (random.nextInt(100) < 40) items.add(new ItemStack(Material.CHAINMAIL_HELMET, 1, (short) 0));
+		if (random.nextInt(100) < 40) items.add(new ItemStack(Material.CHAINMAIL_CHESTPLATE, 1, (short) 0));
+		if (random.nextInt(100) < 40) items.add(new ItemStack(Material.CHAINMAIL_LEGGINGS, 1, (short) 0));
+		if (random.nextInt(100) < 40) items.add(new ItemStack(Material.CHAINMAIL_BOOTS, 1, (short) 0));
+		if (random.nextInt(100) < 10) items.add(new ItemStack(Material.IRON_HELMET, 1, (short) 0));
+		if (random.nextInt(100) < 10) items.add(new ItemStack(Material.IRON_CHESTPLATE, 1, (short) 0));
+		if (random.nextInt(100) < 10) items.add(new ItemStack(Material.IRON_LEGGINGS, 1, (short) 0));
+		if (random.nextInt(100) < 10) items.add(new ItemStack(Material.IRON_BOOTS, 1, (short) 0));
+		if (random.nextInt(100) < 30) items.add(new ItemStack(Material.FLINT, 3, (short) 0));
+		if (random.nextInt(100) < 20) items.add(new ItemStack(Material.FLINT, 5, (short) 0));
+		if (random.nextInt(100) < 10) items.add(new ItemStack(Material.FLINT, 7, (short) 0));
+		if (random.nextInt(100) < 80) items.add(new ItemStack(Material.PORK, 1, (short) 0));
+		if (random.nextInt(100) < 10) items.add(new ItemStack(Material.GRILLED_PORK, 1, (short) 0));
+		if (random.nextInt(100) < 15) items.add(new ItemStack(Material.REDSTONE, 5, (short) 0));
+		if (random.nextInt(100) < 10) items.add(new ItemStack(Material.REDSTONE, 8, (short) 0));
+		if (random.nextInt(100) < 5) items.add(new ItemStack(Material.REDSTONE, 13, (short) 0));
+		if (random.nextInt(100) < 3) items.add(new ItemStack(Material.REDSTONE, 21, (short) 0));
+		if (random.nextInt(100) < 10) items.add(new ItemStack(Material.COMPASS, 1, (short) 0));
+		if (random.nextInt(100) < 80) items.add(new ItemStack(Material.RAW_FISH, 1, (short) 0));
+		if (random.nextInt(100) < 20) items.add(new ItemStack(Material.COOKED_FISH, 1, (short) 0));
+		if (random.nextInt(100) < 20) items.add(new ItemStack(Material.INK_SACK, 1, (short) 3));
+		if (random.nextInt(100) < 5) items.add(new ItemStack(Material.CAKE, 1, (short) 0));
+		if (random.nextInt(100) < 80) items.add(new ItemStack(Material.COOKIE, 3, (short) 0));
+		if (random.nextInt(100) < 20) items.add(new ItemStack(Material.COOKIE, 5, (short) 0));
+
 		int itemCountInChest;
 		switch (random.nextInt(8)) {
 		case 0:
@@ -220,22 +171,22 @@ public class ChestPopulator extends MazeRoomBlockPopulator {
 		default:
 			itemCountInChest = 3;
 		}
-		
+
 		// Increase item size depending on depth
 		itemCountInChest = itemCountInChest + extraLoot;
-			
+
 		// Create a list of item contents with the right amount of items
-		List<ItemStack> newContents = new ArrayList<>();
+		List < ItemStack > newContents = new ArrayList < >();
 		for (int i = 0; i < itemCountInChest; i++)
-			newContents.add(items.get(random.nextInt(items.size())));
+		newContents.add(items.get(random.nextInt(items.size())));
 		return newContents;
 	}
 
-    @Override
-    public float getRoomChance() {
-        return ROOM_CHANCE;
-    }
-	
+	@Override
+	public float getRoomChance() {
+		return ROOM_CHANCE;
+	}
+
 	/**
 	 * Get the minimum layer
 	 * @return Minimum layer
@@ -244,7 +195,7 @@ public class ChestPopulator extends MazeRoomBlockPopulator {
 	public int getMinimumLayer() {
 		return LAYER_MIN;
 	}
-	
+
 	/**
 	 * Get the maximum layer
 	 * @return Maximum layer
